5e73fec2def954605727ec47eee9d820c12c0311
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-09-07 02:21:28 -0700

    Added Component index file

---
+++ pro_portfolio/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Hero } from "./Hero";
+export { default as MySkills } from "./MySkills";
+export { default as Navbar } from "./Navbar";
+export { default as Container } from "./Container";
+export { default as Logo } from "./Logo";
