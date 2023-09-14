59443fdcf80a0b78493dd2daede83dddb105e4f7
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-09-13 17:54:16 -0700

    Created New Button component

---
+++ pro_portfolio/components/MyWorkButton.tsx
@@ -0,0 +1,9 @@
+const MyWorkButton = () => {
+  return (
+    <div className="bg-primary-dark rounded-2xl py-2 md:py-5 px-8 md:px-12 md:rounded-3xl">
+      <button className="text-[#FFFF] truncate">My Work</button>
+    </div>
+  );
+};
+
+export default MyWorkButton;
