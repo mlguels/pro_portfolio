e03da390d35c53451ce867e02aca20f89f724cdc
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-09-14 23:16:11 -0700

    Added Copy Functionality

--- pro_portfolio/components/Email.tsx
+++ pro_portfolio/components/Email.tsx
@@ -1,17 +1,45 @@
+"use client";
+
+import { useRef } from "react";
 import Image from "next/image";
 
-const Email = () => {
+const Email: React.FC = () => {
+  const emailRef = useRef<HTMLInputElement | null>(null);
+
+  const handleCopyEmail = () => {
+    if (emailRef.current) {
+      const emailText = emailRef.current.value;
+
+      navigator.clipboard
+        .writeText(emailText)
+        .then(() => {
+          alert("Email copied to clipboard: " + emailText);
+        })
+        .catch((err) => {
+          console.error("Error copying email to clipboard: ", err);
+        });
+    }
+  };
+
   return (
-    <p className="text-[#6F74A7] flex justify-between items-center gap-3 text-lg font-bold py-4 px-5 rounded-2xl bg-[#FFFF] hover:cursor-copy">
+    <p className="text-[#6F74A7] flex justify-between items-center gap-3 text-lg font-bold py-4 px-5 rounded-2xl bg-[#FFFF]">
+      <input
+        ref={emailRef}
+        type="text"
+        defaultValue="mrodandres@icloud.com"
+        className="hidden"
+      />
       mrodandres@icloud.com
       <span>
-        <Image
-          className="min-w-fit"
-          src="/emailVector.svg"
-          width={15}
-          height={5}
-          alt="copy logo"
-        />
+        <button onClick={handleCopyEmail}>
+          <Image
+            className="min-w-fit"
+            src="/emailVector.svg"
+            width={15}
+            height={5}
+            alt="copy logo"
+          />
+        </button>
       </span>
     </p>
   );
