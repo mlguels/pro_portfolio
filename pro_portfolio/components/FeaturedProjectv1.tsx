b07edffcff871c663180be23c04e8c475c069369
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-09-17 00:02:25 -0700

    Created Interface for project banner

--- pro_portfolio/components/FeaturedProjectv1.tsx
+++ pro_portfolio/components/FeaturedProjectv1.tsx
@@ -1,4 +1,29 @@
-const FeaturedProjectv1 = () => {
+interface desktopImage {
+  url: string;
+  width: number;
+  height: number;
+  alt: string;
+}
+interface mobileImage {
+  url: string;
+  width: number;
+  height: number;
+  alt: string;
+}
+
+interface FeaturedProjectv1Props {
+  projectTitle: string;
+  technologies: string;
+  desktopView: desktopImage;
+  mobileView: mobileImage;
+}
+
+const FeaturedProjectv1: React.FC<FeaturedProjectv1Props> = ({
+  projectTitle,
+  technologies,
+  desktopView,
+  mobileView,
+}) => {
   return <div>FeaturedProjectv1</div>;
 };
 
