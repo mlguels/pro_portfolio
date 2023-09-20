a1f8de15f11cf1e11915f54aa304beaf98f30b64
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-09-19 20:07:26 -0700

    Styled Featured Section Components

--- pro_portfolio/components/FeaturedProjects.tsx
+++ pro_portfolio/components/FeaturedProjects.tsx
@@ -1,21 +1,47 @@
 import Image from "next/image";
 
+import { FeaturedProjectv1, Technologies } from ".";
+
 const FeaturedProjects = () => {
   return (
-    <main>
-      <section>
-        <ul>
-          <li>
-            <Image
-              className="w-[430px] h-auto"
-              src="featuredprojects.svg"
-              width={200}
-              height={100}
-              alt="Featured Projects"
-            />
-          </li>
-        </ul>
-      </section>
+    <main className="h-screen flex justify-center pt-20">
+      <ul className="flex flex-col gap-10 items-center">
+        <li>
+          <Image
+            className="w-[430px] h-auto"
+            src="featuredprojects.svg"
+            width={200}
+            height={100}
+            alt="Featured Projects"
+          />
+        </li>
+        <li>
+          <FeaturedProjectv1
+            projectTitle={
+              <span>
+                Morrent - A Car <br />
+                <span style={{ whiteSpace: "nowrap" }}>Rental Application</span>
+              </span>
+            }
+            technologies={[
+              <Technologies tech1="ReactJS" tech2="Next.js" />,
+              <Technologies tech1="Node.js" tech2="MongoDB" />,
+            ]}
+            desktopView={{
+              url: "/morentLaptop.svg",
+              width: 100,
+              height: 50,
+              alt: "Morent Logo",
+            }}
+            mobileView={{
+              url: "/morentMobile.svg",
+              width: 100,
+              height: 50,
+              alt: "Morent Logo",
+            }}
+          />
+        </li>
+      </ul>
     </main>
   );
 };
--- pro_portfolio/components/FeaturedProjectv1.tsx
+++ pro_portfolio/components/FeaturedProjectv1.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 interface desktopImage {
   url: string;
   width: number;
@@ -12,8 +14,8 @@ interface mobileImage {
 }
 
 interface FeaturedProjectv1Props {
-  projectTitle: string;
-  technologies: string;
+  projectTitle: React.ReactNode;
+  technologies: React.ReactNode[];
   desktopView: desktopImage;
   mobileView: mobileImage;
 }
@@ -24,7 +26,47 @@ const FeaturedProjectv1: React.FC<FeaturedProjectv1Props> = ({
   desktopView,
   mobileView,
 }) => {
-  return <div>FeaturedProjectv1</div>;
+  return (
+    <main className="bg-[#416CEA] w-auto h-auto lg:w-[1200px] lg:h-[444px] rounded-2xl">
+      <section className="flex flex-col md:flex-row items-center p-10">
+        <section>
+          <ul className="flex flex-col p-8 gap-8">
+            <li className="text-5xl text-[#FFFF] font-bold">{projectTitle}</li>
+            <li className="flex flex-col md:flex-row gap-4">
+              {technologies.map((tech, index) => (
+                <div key={index}>{tech}</div>
+              ))}
+            </li>
+            <li>
+              <button>See Detail Project</button>
+            </li>
+          </ul>
+        </section>
+        <section className="">
+          <ul className="flex items-center md:gap-10 mt-12">
+            <li>
+              <Image
+                className="w-auto h-auto"
+                src={desktopView.url}
+                width={desktopView.width}
+                height={desktopView.height}
+                alt={desktopView.alt}
+              />
+            </li>
+            <li className="md:-mx-10">
+              <Image
+                className="w-auto h-auto"
+                src={mobileView.url}
+                width={mobileView.width}
+                height={mobileView.height}
+                alt={mobileView.alt}
+              />
+            </li>
+          </ul>
+        </section>
+      </section>
+    </main>
+  );
 };
 
 export default FeaturedProjectv1;
