4bb53a6df29f8de0b33c2583068aee45098b5698
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-10-02 22:34:00 -0700

    Linked Socials

--- pro_portfolio/components/Footer.tsx
+++ pro_portfolio/components/Footer.tsx
@@ -4,18 +4,35 @@ const Footer = () => {
   return (
     <main className="pt-14 pb-14 pl-16 pr-16">
       <section className="flex justify-between">
-        <p className="text-[#778295]">© 2023 Miguel. All rights reserved</p>
+        <p className="text-[#778295]"> © 2023 Miguel</p>
         <ul className="flex gap-3">
           <li>
-            <Image src="/linkedin.svg" width={20} height={10} alt="LinkedIn" />
+            <a
+              href="https://www.linkedin.com/in/miguel-andres-rodriguez/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                src="/linkedin.svg"
+                width={20}
+                height={10}
+                alt="LinkedIn"
+              />
+            </a>
           </li>
           <li>
-            <Image
-              src="/githubIcon.svg"
-              width={20}
-              height={10}
-              alt="LinkedIn"
-            />
+            <a
+              href="https://github.com/mlguels"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                src="/githubIcon.svg"
+                width={20}
+                height={10}
+                alt="LinkedIn"
+              />
+            </a>
           </li>
         </ul>
       </section>
