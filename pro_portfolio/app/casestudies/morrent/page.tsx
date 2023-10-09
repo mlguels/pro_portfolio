511b0d2b702e9f8e55dde97c9eb497aef32bebaa
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-10-08 22:38:36 -0700

    Added Demo & Source code links

--- pro_portfolio/app/casestudies/morrent/page.tsx
+++ pro_portfolio/app/casestudies/morrent/page.tsx
@@ -30,23 +30,36 @@ const MorrentCaseStudy = () => {
             />
           </li>
           <li className="flex gap-10 md:gap-44">
-            <Image
-              src="/assets/demosite.svg"
-              width={150}
-              height={100}
-              alt="Title"
-            />
+            <a
+              href="https://cohort3-jupiter-car-rent.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                src="/assets/demosite.svg"
+                width={150}
+                height={100}
+                alt="Title"
+              />
+            </a>
 
-            <Image
-              src="/assets/sourcecode.svg"
-              width={160}
-              height={100}
-              alt="Title"
-            />
+            <a
+              href="https://github.com/JavaScript-Mastery-PRO/Cohort3_Jupiter_CarRent"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <Image
+                src="/assets/sourcecode.svg"
+                width={160}
+                height={100}
+                alt="Title"
+              />
+            </a>
           </li>
         </ul>
       </section>
 
+      {/*Role*/}
       <section className="p-14">
         <ul className="flex flex-col md:flex-row justify-center md:gap-36">
           <li className="flex flex-col gap-1">
@@ -60,6 +73,34 @@ const MorrentCaseStudy = () => {
           <li className="flex flex-col gap-1">
             <p className="text-[#778295]">End Date</p>
             <h1 className="font-semibold text-xl">04/01/2023</h1>
+          </li>
+        </ul>
+      </section>
+
+      {/* Techstack */}
+      <section>
+        <ul className="text-center">
+          <li>
+            <p className="text-[#0252CD] text-xs font-semibold">
+              Technologies used
+            </p>
+          </li>
+          <li>
+            <h2 className="text-2xl font-semibold">Tech Stack</h2>
+          </li>
+        </ul>
+        <ul className="flex justify-center">
+          <li>
+            <Image src="/react.svg" width={100} height={50} alt="react" />
+          </li>
+          <li>
+            <Image src="/next.svg" width={100} height={50} alt="react" />
+          </li>
+          <li>
+            <Image src="/tailwind.svg" width={100} height={50} alt="react" />
+          </li>
+          <li>
+            <Image src="/ts.svg" width={100} height={50} alt="react" />
           </li>
         </ul>
       </section>
