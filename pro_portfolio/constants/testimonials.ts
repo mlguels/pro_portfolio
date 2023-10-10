8e4124e18d51bbc47cbbe92de604e8ef448bdd8b
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-10-09 23:08:21 -0700

    Added testimonials data

---
+++ pro_portfolio/constants/testimonials.ts
@@ -0,0 +1,38 @@
+export const testimonials = [
+  {
+    image: {
+      url: "/Adrian.svg",
+      width: 200,
+      height: 200,
+      alt: "Adrian",
+    },
+    testimony:
+      "I highly recommend Miguel. He's not only respectful and honest but also dedicated to his work. His consistent growth is truly impressive!",
+    name: "Adrian Hajdin",
+    info: "Founder & CEO at JS Mastery",
+  },
+  {
+    image: {
+      url: "/mateo.jpeg",
+      width: 200,
+      height: 200,
+      alt: "Mateo",
+    },
+    testimony:
+      "I'm pleased to recommend Miguel. He's a strong presence and his commitment to learning is truly inspiring. His dedication to excellence shines through in his work, and he is a valuable asset to any team.",
+    name: "Mateo Sindičić",
+    info: "Software Engineering Lead / Mentor",
+  },
+  {
+    image: {
+      url: "/ben.png",
+      width: 200,
+      height: 200,
+      alt: "Ben",
+    },
+    testimony:
+      "Miguel has demonstrated that there is no upper limit to his capacity for learning. I've witnessed his tremendous contributions to the team and his unwavering dedication. His positive attitude, eagerness for growth, and receptiveness to feedback make him an absolute pleasure to work with and an excellent addition to any team",
+    name: "Ben Pham",
+    info: "Software Engineer",
+  },
+];
