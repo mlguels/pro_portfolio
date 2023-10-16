413e50a0551b1219f0a6579b1b3838aa57bab9c4
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-10-16 07:20:11 -0700

    Added Darkmode icons

--- pro_portfolio/constants/skillsData.ts
+++ pro_portfolio/constants/skillsData.ts
@@ -2,53 +2,66 @@ export const skillsData = [
   {
     name: "React",
     iconPath: "/react.svg",
+    darkIconPath: "/reactDark.svg",
   },
   {
     name: "Javascript",
     iconPath: "/js.svg",
+    darkIconPath: "/jsDark.svg",
   },
   {
     name: "NextJs",
     iconPath: "/next.svg",
+    darkIconPath: "/nextDark.svg",
   },
   {
     name: "Typescript",
     iconPath: "/ts.svg",
+    darkIconPath: "/tsDark.svg",
   },
   {
     name: "Tailwind",
     iconPath: "/tailwind.svg",
+    darkIconPath: "/tailwindDark.svg",
   },
   {
     name: "CSS",
     iconPath: "/css.svg",
+    darkIconPath: "/cssDark.svg",
   },
   {
     name: "Express",
     iconPath: "/express.svg",
+    darkIconPath: "/expressDark.svg",
   },
   {
     name: "Github",
     iconPath: "/github.svg",
+    darkIconPath: "/githubDark.svg",
   },
   {
     name: "HTML",
     iconPath: "/html.svg",
+    darkIconPath: "/htmlDark.svg",
   },
   {
     name: "Mongo",
     iconPath: "/mongo.svg",
+    darkIconPath: "/mongoDark.svg",
   },
   {
     name: "MUI",
     iconPath: "/mui.svg",
+    darkIconPath: "/muiDark.svg",
   },
   {
     name: "Node",
     iconPath: "/node.svg",
+    darkIconPath: "/nodeDark.svg",
   },
   {
     name: "Git",
     iconPath: "/git.svg",
+    darkIconPath: "/gitDark.svg",
   },
 ];
