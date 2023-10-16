b7c32c8a47f92e81e984ad0f003bfc1b07f1bef8
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-10-16 07:20:03 -0700

    Completed Darkmode for Skills

--- pro_portfolio/components/MySkills.tsx
+++ pro_portfolio/components/MySkills.tsx
@@ -3,26 +3,32 @@
 import Image from "next/image";
 
 import { skillsData } from "@/constants/skillsData";
+import { useTheme } from "@/context/theme-context";
 
 const MySkills = () => {
+  const { theme } = useTheme();
   return (
-    <main className="bg-[#FFFF] pb-10">
+    <main className="bg-[#FFFF] pb-10 dark:bg-main-black">
       <div className="flex flex-col items-center gap-5 pt-10">
         <div>
-          <Image src="/TITLE.svg" width={225} height={200} alt="My Skill" />
+          {theme === "light" ? (
+            <Image src="/TITLE.svg" width={225} height={200} alt="My Skill" />
+          ) : (
+            <Image src="/TITLE.png" width={225} height={200} alt="My Skill" />
+          )}
         </div>
-        <ul className="grid grid-cols-4 md:grid-cols-8 gap-10 ">
+        <ul className="grid grid-cols-4 md:grid-cols-8 gap-10">
           {skillsData.map((skill, index) => (
             <li
               className="transition-transform transform hover:scale-110 shadow-sm"
               key={index}
             >
               <Image
-                src={skill.iconPath}
+                src={theme === "dark" ? skill.darkIconPath : skill.iconPath}
                 width={125}
                 height={125}
                 alt="Skill Icon"
-                className="grayscale-img"
+                className={theme === "light" && "grayscale-img"}
               />
             </li>
           ))}
