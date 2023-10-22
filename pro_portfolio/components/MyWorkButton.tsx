05e4f4d71123fd6dc5ab96b829acf627b2ad843c
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-10-22 13:11:26 -0700

    Fixed mobile view

--- pro_portfolio/components/MyWorkButton.tsx
+++ pro_portfolio/components/MyWorkButton.tsx
@@ -1,6 +1,6 @@
 const MyWorkButton = () => {
   return (
-    <div className="bg-primary-dark dark:bg-primary-light rounded-2xl py-2 md:py-5 px-8 md:px-20 md:rounded-3xl text-center hover:cursor-pointer hover:scale-105">
+    <div className="bg-primary-dark dark:bg-primary-light rounded-3xl py-6 px-10 md:w-[240px] text-center">
       <button className="text-[#FFFF] font-semibold text-lg">My Work</button>
     </div>
   );
