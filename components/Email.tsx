09f430c44857d52724ccb1d574847bf600797058
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-10-25 15:16:15 -0700

    Add: React-hot-toast

--- components/Email.tsx
+++ components/Email.tsx
@@ -2,28 +2,31 @@
 
 import { useRef } from "react";
 import Image from "next/image";
+import toast, { Toaster } from "react-hot-toast";
 
 const Email: React.FC = () => {
   const emailRef = useRef<HTMLInputElement | null>(null);
 
   const handleCopyEmail = () => {
     if (emailRef.current) {
-      const emailText = emailRef.current.value;
+      navigator.clipboard.writeText(emailRef.current.value);
+      toast.success("Email Copied", {
+        duration: 2000,
+        position: "top-center",
 
-      navigator.clipboard
-        .writeText(emailText)
-        .then(() => {
-          alert("Email copied to clipboard: " + emailText);
-        })
-        .catch((err) => {
-          console.error("Error copying email to clipboard: ", err);
-        });
+        ariaProps: {
+          role: "status",
+          "aria-live": "polite",
+        },
+      });
     }
   };
 
   return (
-    <p
-      className="
+    <main>
+      <Toaster />
+      <div
+        className="
       text-[#6F74A7] 
       flex 
       justify-between 
@@ -41,26 +44,27 @@ const Email: React.FC = () => {
       hover:opacity-50 
       transition
       "
-    >
-      <input
-        ref={emailRef}
-        type="text"
-        defaultValue="mrodandres@icloud.com"
-        className="hidden"
-      />
-      mrodandres@icloud.com
-      <span className="flex justify-center items-center">
-        <button onClick={handleCopyEmail}>
-          <Image
-            className="min-w-fit"
-            src="/emailVector.svg"
-            width={15}
-            height={5}
-            alt="copy logo"
-          />
-        </button>
-      </span>
-    </p>
+      >
+        <input
+          ref={emailRef}
+          type="text"
+          defaultValue="mrodandres@icloud.com"
+          className="hidden"
+        />
+        mrodandres@icloud.com
+        <div className="flex justify-center items-center">
+          <button onClick={handleCopyEmail}>
+            <Image
+              className="min-w-fit"
+              src="/emailVector.svg"
+              width={15}
+              height={5}
+              alt="copy logo"
+            />
+          </button>
+        </div>
+      </div>
+    </main>
   );
 };
 
