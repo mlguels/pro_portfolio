a0215564235e55658684b28052f765c9ac789b30
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-10-27 15:36:57 -0700

    Added Darkmode

--- components/Challenges.tsx
+++ components/Challenges.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 const Challenges = () => {
   return (
-    <main className="bg-main-bg rounded-lg p-5 md:mx-72">
+    <main className="bg-main-bg rounded-lg p-5 md:mx-72 dark:bg-black">
       <h1 className="text-[#E15A46] text-lg font-semibold">Challenges</h1>
       <div className="flex flex-col gap-5 pt-4">
         <div className="flex gap-4 justify-start items-start">
           <Image src="/assets/Challegne.svg" width={20} height={5} alt="svg" />{" "}
           <span>
-            <p className="text-[#6F74A7]">
+            <p className="text-[#6F74A7] dark:text-[#F3F8FF]">
               Adapted to Next.js 13's evolving structure and app folder
               organization, addressing initial hurdles in project development.
             </p>
@@ -17,7 +17,7 @@ const Challenges = () => {
         <div className="flex gap-4 justify-start items-start">
           <Image src="/assets/Challegne.svg" width={20} height={5} alt="svg" />{" "}
           <span>
-            <p className="text-[#6F74A7]">
+            <p className="text-[#6F74A7] dark:text-[#F3F8FF]">
               Demonstrated problem-solving skills by navigating authentication
               limitations with NextAuth, creatively seeking alternative
               solutions for a smooth application experience.
@@ -27,7 +27,7 @@ const Challenges = () => {
         <div className="flex gap-4 justify-start items-start">
           <Image src="/assets/Challegne.svg" width={20} height={5} alt="svg" />{" "}
           <span>
-            <p className="text-[#6F74A7]">
+            <p className="text-[#6F74A7] dark:text-[#F3F8FF]">
               Maintained resilience and resourcefulness in overcoming setbacks,
               effectively leveraging available resources to achieve project
               success despite challenges posed by the current version of
