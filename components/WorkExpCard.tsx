bf12c70d6af16ca85b2d354f01b5d30e07566ec5
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-10-29 13:16:54 -0700

    Refactored Card

--- components/WorkExpCard.tsx
+++ components/WorkExpCard.tsx
@@ -6,58 +6,31 @@ const content = [
     desc: "Full-Stack Developer - 6 months +",
     url: "/jsmlogl.webp",
   },
-  {
-    title: "Another Job Title",
-    desc: "Another job description.",
-    url: "/jsmlogl.webp",
-  },
-  {
-    title: "Yet Another Job",
-    desc: "This is the description for another job.",
-    url: "/jsmlogl.webp",
-  },
-  {
-    title: "One More Job",
-    desc: "This is the description for one more job.",
-    url: "/jsmlogl.webp",
-  },
 ];
 
-interface ImageLogo {
-  url: string;
-  width: number;
-  height: number;
-  alt: string;
-}
+// interface ImageLogo {
+//   url: string;
+//   width: number;
+//   height: number;
+//   alt: string;
+// }
 
-interface WorkExpCardProps {
-  imageLogo: ImageLogo;
-  title: string;
-  desc: string;
-  show: boolean;
-}
+interface WorkExpCardProps {}
 
-const WorkExpCard: React.FC<WorkExpCardProps> = ({
-  imageLogo,
-  title,
-  desc,
-}) => {
+const WorkExpCard: React.FC<WorkExpCardProps> = ({}) => {
   return (
     <main className="bg-[#F3F8FF] flex w-[400px] h-[150px] md:w-[607px] md:h-[120px] rounded-2xl hover:shadow-lg hover:scale-105 hover:translate-x-10 duration-300 ease-in-out">
-      <ul className="flex items-center p-6 justify-start gap-4">
-        <li>
-          <Image
-            src={imageLogo.url}
-            width={imageLogo.width}
-            height={imageLogo.height}
-            alt={imageLogo.alt}
-          />
-        </li>
-        <li>
-          <h3 className="text-xl font-bold">{title}</h3>
-          <p>{desc}</p>
-        </li>
-      </ul>
+      {content.map((job, key) => (
+        <ul key={key} className="flex items-center p-6 justify-start gap-4">
+          <li>
+            <Image src={job.url} width={50} height={50} alt="Logo" />
+          </li>
+          <li>
+            <h3 className="text-xl font-bold">{job.title}</h3>
+            <p>{job.desc}</p>
+          </li>
+        </ul>
+      ))}
     </main>
   );
 };
