944688604b424ff88178bf361e64891fa7459bf1
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2023-11-04 19:51:33 -0700

    Added: Work constants

---
+++ constants/work.ts
@@ -0,0 +1,20 @@
+export const workExperienceContent = [
+  {
+    title: "Work Experience",
+    description1:
+      "Take a trip through my career, where I&posve not only paved the way butalso reached important goals, taken on different, worked on impressiveand accomplished notable things.",
+    description2:
+      "From coding in the late hours to collaborating with talented teams, my career has been a dynamic adventure filled with growth and innovation",
+    imageLight: "/slide.svg",
+    imageDark: "/slideDar.svg",
+  },
+  {
+    title: "JSM Pro Masterclass",
+    description1:
+      "JSM Masterclass experience provided me with 6 months of work experience collaborating with a team of developers in comprehensive projects.",
+    description2:
+      "During the process of creating those projects, mentors gave me feedback on my code. With each pull request I pushed I began to better my code.",
+    imageLight: "/slide2.svg",
+    imageDark: "/slideDar2.svg",
+  },
+];
