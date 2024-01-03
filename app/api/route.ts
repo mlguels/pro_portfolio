12bceac2880354efdb009b3a77ae6dc09335c0bd
Miguel Rodriguez <104812366+mlguels@users.noreply.github.com>
2024-01-02 20:47:26 -0800

    Refactored route

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,9 +1,33 @@
-import { NextApiRequest, NextApiResponse } from "next";
+// import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+// export default async function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) {
+//   if (req.method === "POST") {
+//     try {
+//       const response = await fetch("https://formspree.io/f/xjvqjvqd", {
+//         method: "POST",
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//         body: JSON.stringify(req.body),
+//       });
+
+//       const data = await response.json();
+//       return new Response(JSON.stringify(data));
+//     } catch (error) {
+//       console.error(error);
+//       return new Response("Internal server error", { status: 500 });
+//     }
+//   } else {
+//     return new Response("Error sending message", { status: 400 });
+//   }
+// }
+
+import { NextApiHandler } from "next";
+
+const handler: NextApiHandler = async (req, res) => {
   if (req.method === "POST") {
     try {
       const response = await fetch("https://formspree.io/f/xjvqjvqd", {
@@ -15,12 +39,14 @@ export default async function handler(
       });
 
       const data = await response.json();
-      return new Response(JSON.stringify(data));
+      res.status(200).json(data);
     } catch (error) {
       console.error(error);
-      return new Response("Internal server error", { status: 500 });
+      res.status(500).send("Internal server error");
     }
   } else {
-    return new Response("Error sending message", { status: 400 });
+    res.status(400).send("Error sending message");
   }
-}
+};
+
+export default handler;
